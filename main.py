9ab1d0d4a3cd209cb9331674d027a0c9055911a4
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-05-03 23:51:13 -0400

    Main Ship Config & Controls

--- main.py
+++ main.py
@@ -26,12 +26,28 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 # Background Image
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
+class Ship():
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, (250,0,0), (self.x, self.y, 50, 50))
+        
+
 def main():
     run = True
     FPS = 60
     level = 1
     lives = 5
     main_font = pygame.font.SysFont("Helvetica Neue Light", 50)
+    player_vel = 5
+    ship = Ship(300, 650)
 
     clock = pygame.time.Clock()
 
@@ -44,14 +60,28 @@ def main():
         WIN.blit(lives_label, (10,10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
+        ship.draw(WIN)
+
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
         redraw_window()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]: # Left
+            ship.x -= player_vel
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]: # Right
+            ship.x += player_vel
+        if keys[pygame.K_w] or keys[pygame.K_UP]: # Up
+            ship.y -= player_vel
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]: # Down
+            ship.y += player_vel
+
             
 
 main()
