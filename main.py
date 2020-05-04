07a887adea8b31851d2f68a460522a7210cadc4b
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-05-03 22:51:36 -0400

    Add WIndow

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 import time
 import random
 
+# Window Config Settings
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Invaders")
+
 # Load Images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
